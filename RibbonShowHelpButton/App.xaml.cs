fc31ddd976647ef130621fe5dbf80bea6e7310e2
Elakkiya1730 <70741355+Elakkiya1730@users.noreply.github.com>
2021-02-12 16:39:33 +0530

    Added sample

---
+++ RibbonShowHelpButton/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WPFRibbonApplicationMenu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+        private void HelpButton_Click_1(object sender, RoutedEventArgs e)
+        {
+           //navigate to url link
+            System.Diagnostics.Process.Start("https://help.syncfusion.com/wpf/welcome-to-syncfusion-essential-wpf");
+        }
+    }
+}
